11ec80f9ec3e61377c49b6bf0a9556142389e68e
evgeniach <evgenia.cheremnyh@gmail.com>
2023-04-25 21:02:38 +0700

    Ex4: GET-запрос

--- src/test/java/SecondTest_Ex4.java
+++ src/test/java/SecondTest_Ex4.java
@@ -2,13 +2,13 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-public class secondTest {
+public class SecondTest_Ex4 {
 
     @Test
     public void testHellowWorld() {
 
         Response response= RestAssured
-                .get("https://playground.learnqa.ru/api/hello")
+                .get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
         response.prettyPrint();
     }
