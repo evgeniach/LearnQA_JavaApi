125167ab8f010035dcf24337b8c3db89967bee75
evgeniach <evgenia.cheremnyh@gmail.com>
2023-05-02 12:12:23 +0700

    Ex7: Долгий редирект

--- src/test/java/HomeWork_Ex7.java
+++ src/test/java/HomeWork_Ex7.java
@@ -7,16 +7,29 @@ public class HomeWork_Ex7 {
     @Test
     public void Ex7() {
 
-        Response response= RestAssured
-                .given()
-                .redirects()
-                .follow(false)
-                .when()
-                .get("https://playground.learnqa.ru/api/long_redirect")
-                .andReturn();
+        int statusCode = 0;
+        String response_url="https://playground.learnqa.ru/api/long_redirect";
+        int countsRedirects = 0;
 
-        String locationHeader = response.getHeader("Location");
-        System.out.println("Location for redirect: "+locationHeader);
+        while(statusCode!=200) {
+
+            Response response= RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(response_url)
+                    .andReturn();
+
+            statusCode = response.getStatusCode();
+
+            if (statusCode==301) {
+                countsRedirects++;
+                response_url = response.getHeader("Location");
+                System.out.println("Location for redirect: "+response_url);
+            }
+        }
+        System.out.println("Count of redirects: "+countsRedirects);
     }
 
 }
