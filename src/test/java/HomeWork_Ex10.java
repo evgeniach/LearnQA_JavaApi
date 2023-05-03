22d45f11b5cca83f0946ad1241ffef22b0863a30
evgeniach <evgenia.cheremnyh@gmail.com>
2023-05-03 17:51:53 +0700

    Ex10: Тест на короткую фразу

---
+++ src/test/java/HomeWork_Ex10.java
@@ -0,0 +1,24 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HomeWork_Ex10 {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"","abcdefghijklmn","abcdefghijklmno","abcdefghijklmnop"})
+    public void testHelloMethodNameWithParams(String text) {
+
+        assertTrue(text.length()>15,"Text length less than 15");
+    }
+}
