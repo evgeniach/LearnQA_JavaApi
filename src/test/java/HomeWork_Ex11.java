593db21aa67422411d23d925a1888fdaf5ebe0b8
evgeniach <evgenia.cheremnyh@gmail.com>
2023-05-09 12:06:32 +0700

    fix test name

--- src/test/java/HomeWork_Ex11.java
+++ src/test/java/HomeWork_Ex11.java
@@ -11,11 +11,11 @@ public class HomeWork_Ex11 {
     @Test
     public void testAuthUser() {
 
-        Response responseGetAuth = RestAssured
+        Response responseGetCookie = RestAssured
                 .get("https://playground.learnqa.ru/api/homework_cookie")
                 .andReturn();
 
-        Map<String, String> cookies = responseGetAuth.getCookies();
+        Map<String, String> cookies = responseGetCookie.getCookies();
 
         assertTrue(cookies.containsKey("HomeWork"), "Response doesn't have 'HomeWork' cookie");
 
--- src/test/java/HomeWork_Ex12.java
+++ src/test/java/HomeWork_Ex12.java
@@ -12,11 +12,11 @@ public class HomeWork_Ex12 {
     @Test
     public void testAuthUser() {
 
-        Response responseGetAuth = RestAssured
+        Response responseGetHeader = RestAssured
                 .get("https://playground.learnqa.ru/api/homework_header")
                 .andReturn();
 
-        Headers headers = responseGetAuth.getHeaders();
+        Headers headers = responseGetHeader.getHeaders();
 
         assertTrue(headers.hasHeaderWithName("x-secret-homework-header"),"Response doesn't have 'x-secret-homework-header' header");
         assertTrue(headers.getValue("x-secret-homework-header").equals("Some secret value"),"Response header is not equal to 'Some secret value'");
